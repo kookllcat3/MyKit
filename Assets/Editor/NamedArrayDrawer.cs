1604e5132e63edbb75655c2c1169c5cbec8c5692
tommy <kookllcat3@gmail.com>
2020-02-06 11:21:17 +0800

    可以複寫array的index名稱

---
+++ Assets/Editor/NamedArrayDrawer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(NamedArrayAttribute))]
+public class NamedArrayDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+    {
+        try
+        {
+            var s = property.propertyPath.Split('[', ']');
+            int pos = int.Parse(s[s.Length - 2]);
+            EditorGUI.ObjectField(rect, property, new GUIContent(((NamedArrayAttribute)attribute).names[pos]));
+        }
+        catch
+        {
+            EditorGUI.ObjectField(rect, property, label);
+        }
+    }
+}
