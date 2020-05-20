e3cbd4a8cf474bdbfcb557896d01c88cc0b03800
tommy <kookllcat3@gmail.com>
2020-05-20 10:47:53 +0800

    add WebInputField

---
+++ Assets/WebInputField/Plugins/WebInputField.jslib
@@ -0,0 +1,12 @@
+﻿mergeInto(LibraryManager.library, {
+
+    InputBox: function (str, callbackObj, callbackFunc) {
+        var msg = prompt("Please enter message", Pointer_stringify(str));
+        if (msg != null && callbackObj != null && callbackFunc != null) {
+            SendMessage(Pointer_stringify(callbackObj), Pointer_stringify(callbackFunc), msg);
+        }
+    },
+
+
+});
+
---
+++ Assets/WebInputField/WebInputField.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+using UnityEngine.EventSystems;
+
+
+public class WebInputField : MonoBehaviour, IPointerClickHandler
+{
+    [DllImport("__Internal")]
+    static extern void InputBox(string msg, string callbackObj, string callbackFunc);
+
+    InputField inputField;
+
+    void Start()
+    {
+        inputField = GetComponent<InputField>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR    
+        InputBox(inputField.text, gameObject.name, "Callback");
+#endif
+    }
+
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+    void Callback(string str)
+    {
+        inputField.text = str;
+    }
+#endif
+}
