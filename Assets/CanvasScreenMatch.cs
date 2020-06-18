73c658ad854eb3393f5517081b42abcbe6afbce7
tommy <kookllcat3@gmail.com>
2020-06-18 17:06:47 +0800

    add CanvasScreenMatch

---
+++ Assets/CanvasScreenMatch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasScreenMatch : MonoBehaviour
+{
+    void Awake()
+    {
+        var canvas = GetComponent<CanvasScaler>();
+        Vector2 size = new Vector2(Screen.width / canvas.referenceResolution.x, Screen.height / canvas.referenceResolution.y);
+        canvas.matchWidthOrHeight = size.y > size.x ? 0 : 1;
+    }
+}
